640fed4a004750fc95e32e4d53ee9048b817eafe
kg0ez <82456927+kg0ez@users.noreply.github.com>
2022-08-02 17:09:48 +0300

    perser

---
+++ Pars/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace Pars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            HtmlWeb web = new HtmlWeb();
+            web.OverrideEncoding = Encoding.UTF8;
+            HtmlDocument document = web.Load("https://www.belta.by/all_news");
+            ArrayList list = new ArrayList();
+            int count = 0;
+            foreach (HtmlNode node in document.DocumentNode.SelectNodes("//div[contains(@class, 'lenta_inner')]//a[@href]"))
+            {
+                count++;
+                Console.WriteLine(node.GetAttributeValue("href", null));
+                if(count>2)
+                    list.Add(node.GetAttributeValue("href", null));
+                if (count == 12) break;
+            }
+            foreach (string item in list)
+            {
+                document = web.Load(item);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                foreach (HtmlNode link in document.DocumentNode.SelectNodes("//div[contains(@class, 'content_margin')]//h1"))
+                    Console.WriteLine(link.InnerText);
+
+                Console.ForegroundColor = ConsoleColor.White;
+                foreach (HtmlNode link in document.DocumentNode.SelectNodes("//div[contains(@class, 'js-mediator-article')]//p"))
+                    Console.WriteLine(link.InnerText);
+            }
+        }
+    }
+}
+
